99eb60294dc6f6867c852aeeec927f2b6c7e762f
SumitOP787 <82710581+SumitOP787@users.noreply.github.com>
2022-06-07 12:13:51 +0530

    Update voltage_RMX3360.mk

--- voltage_RMX3360.mk
+++ voltage_RMX3360.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/oplus/RMX3360/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
+# Official-ify
+VOLTAGE_BUILD_TYPE := OFFICIAL
+
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
